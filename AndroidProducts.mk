eb507ec91d25fb1631d7001f3b35120464fc6662
Arda Şen <asen436@gmail.com>
2024-02-22 22:11:01 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,11 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk \
-	$(LOCAL_DIR)/lineage_marlin.mk \
-	$(LOCAL_DIR)/lineage_sailfish.mk
+	$(LOCAL_DIR)/evolution_marlin.mk \
+	$(LOCAL_DIR)/evolution_sailfish.mk
 
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
 	aosp_sailfish-userdebug
+	evolution_marlin-userdebug \
+	evolution_sailfish-userdebug
