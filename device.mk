eeb34a8672d21de998ffbecc3cfb7a9f876a4cee
Arda Şen <asen436@gmail.com>
2024-02-22 22:52:06 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,10 +1,4 @@
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-evolution
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
