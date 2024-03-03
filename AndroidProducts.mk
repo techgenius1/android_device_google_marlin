72807f8b0db1cc52fe2fbca49d9ef229edd07bba
Arda Şen <asen436@gmail.com>
2024-03-03 12:31:28 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,6 +6,6 @@ PRODUCT_MAKEFILES := \
 
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
-	aosp_sailfish-userdebug
+	aosp_sailfish-userdebug \
 	evolution_marlin-userdebug \
 	evolution_sailfish-userdebug
