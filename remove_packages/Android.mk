e96a61bcc4278a236b9a1c96926a8c0cfa5206d9
techgenius1 <asen436@gmail.com>
2025-02-04 02:59:54 +0300

    Remove BCR and Accord

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore BetterBugStub CbrsNetworkMonitor ConnMO DCMO DevicePolicyPrebuilt DiagnosticsToolPrebuilt DMService Drive GCS GoogleTTS MaestroPrebuilt Maps PixelWallpapers2022 PlayAutoInstallConfig PrebuiltGmail RecorderPrebuilt SCONE ScribePrebuilt SCONE-v37764 Showcase SoundAmplifierPrebuilt SprintHM talkback Tycho VZWAPNLib VzwOmaTrigger WfcActivation
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore BetterBugStub bcr CbrsNetworkMonitor ConnMO DCMO DevicePolicyPrebuilt DiagnosticsToolPrebuilt DMService Drive GCS GoogleTTS MaestroPrebuilt Maps PixelWallpapers2022 PlayAutoInstallConfig PrebuiltGmail RecorderPrebuilt SCONE ScribePrebuilt SCONE-v37764 Showcase SoundAmplifierPrebuilt SprintHM talkback Tycho VZWAPNLib VzwOmaTrigger WfcActivation
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
